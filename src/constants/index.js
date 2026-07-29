9feb73654300556cc693474d53fcda971633acd1
afy861345 <afy861345@yahoo.com>
2026-07-29 13:13:16 +0300

    Work

--- src/constants/index.js
+++ src/constants/index.js
@@ -255,3 +255,33 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
     targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -9, -10] : isTablet ? [-11, -7, -10] : [-13, -10, -10],
   };
 };
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Framer',
+    pos: 'Lead Web Developer',
+    duration: '2022 - Present',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Figma',
+    pos: 'Web Developer',
+    duration: '2020 - 2022',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Notion',
+    pos: 'Junior Web Developer',
+    duration: '2019 - 2020',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
